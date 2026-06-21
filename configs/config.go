6f980ce22189ad20eda73776d25ce0b1f3d8beb0
mahdihp.devsc <messenger123456>
2026-06-21 21:07:06 +0330

    add config system

--- configs/config.go
+++ configs/config.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/knadh/koanf/providers/env"
+	"github.com/knadh/koanf/parsers/dotenv"
+	"github.com/knadh/koanf/providers/file"
 	"github.com/knadh/koanf/v2"
 )
 
 type Config struct {
 	MessagePlatform MessagePlatform `koanf:"mp"`
-	Database        Database        `koanf:"mp.db"`
+	Database        Database        `koanf:"db"`
 }
 
 type MessagePlatform struct {
@@ -27,23 +28,37 @@ type Database struct {
 	Password string `koanf:"password"`
 }
 
+var k = koanf.New(".")
+
 func LoadConfig() {
 
-	k := koanf.New(".")
-	err := k.Load(env.Provider("MP_", ".", func(s string) string {
-		s = strings.TrimPrefix(s, "MP_")
-		s = strings.ToLower(s)
-		return strings.ReplaceAll(s, "_", ".")
-	}), nil)
+	err := k.Load(
+		file.Provider(".env"),
+		dotenv.Parser(),
+	)
 
 	if err != nil {
 		panic(err)
 	}
+	nk := koanf.New(".")
 
-	fmt.Printf("%#v\n", k.All())
+	for k1, v := range k.All() {
+
+		//if strings.TrimPrefix(k1, "MP_DB_")  {
+		//
+		//}
+		key := strings.ToLower(strings.TrimPrefix(k1, "MP_"))
+		key = strings.ReplaceAll(key, "_", ".")
+
+		nk.Set(key, v)
+	}
+	//fmt.Printf("%#v\n", nk.All())
 	var cfg Config
 
-	if err := k.Unmarshal("", &cfg); err != nil {
+	if err := nk.Unmarshal("", &cfg.MessagePlatform); err != nil {
+		panic(err)
+	}
+	if err := nk.Unmarshal("", &cfg.Database); err != nil {
 		panic(err)
 	}
 
--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.26.0
 
 require (
 	entgo.io/ent v0.14.6
-	github.com/knadh/koanf/providers/env v1.1.0
+	github.com/knadh/koanf/parsers/dotenv v1.1.1
+	github.com/knadh/koanf/providers/env/v2 v2.0.0
+	github.com/knadh/koanf/providers/file v1.2.1
 	github.com/knadh/koanf/v2 v2.3.5
 	github.com/lib/pq v1.12.3
 )
@@ -14,11 +16,13 @@ require (
 	github.com/agext/levenshtein v1.2.3 // indirect
 	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
 	github.com/bmatcuk/doublestar v1.3.4 // indirect
+	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/go-openapi/inflect v0.19.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.5.0 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/hcl/v2 v2.18.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/knadh/koanf/maps v0.1.2 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
@@ -27,6 +31,7 @@ require (
 	github.com/zclconf/go-cty v1.14.4 // indirect
 	github.com/zclconf/go-cty-yaml v1.1.0 // indirect
 	golang.org/x/mod v0.24.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
