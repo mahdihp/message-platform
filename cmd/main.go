59ef93d8f061fbcb1007fe3b5cd74f4bfb334734
mahdihp.devsc <messenger123456>
2026-06-22 13:43:14 +0330

    add config system 2.1

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,35 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"message-platform/configs"
+	"message-platform/ent"
 
 	_ "github.com/lib/pq"
 )
 
-func main() {
-	//client, err := ent.Open("postgres", "host=localhost port=5432 user=admin dbname=mydb password=123456 sslmode=disable")
-	//if err != nil {
-	//	log.Fatalf("failed opening connection to postgres: %v", err)
-	//}
-	//defer client.Close()
-	//// Run the auto migration tool.
-	//if err := client.Schema.Create(context.Background()); err != nil {
-	//	log.Fatalf("failed creating schema resources: %v", err)
-	//}
-	//
-	//fmt.Println("wwwwwwww")
+func init() {
 
-	configs.LoadConfig()
+}
+
+func main() {
+	system_Config := configs.LoadConfig()
+	setupDB(system_Config)
+
+}
+
+func setupDB(system_Config configs.Config) {
+	connectionString := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
+		system_Config.Database.Host, system_Config.Database.Port, system_Config.Database.Username, system_Config.Database.Name, system_Config.Database.Password)
+	client, err := ent.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatalf("failed opening connection to postgres: %v", err)
+	}
+	defer client.Close()
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
 }
